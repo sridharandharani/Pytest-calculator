a7a05b61b58ad270b4e4439951fd9cb7a80d023c
sridharandharani <sridharandharani1815@gmail.com>
2022-03-29 10:33:31 +0530

    parameterize

--- test_calculator.py
+++ test_calculator.py
@@ -1,32 +1,24 @@
 import calculator
 import pytest
 
-@pytest.mark.addsub
-def test_add():
-    x = 10
-    y = 20
+@pytest.mark.parametrize("x,y,z",[(5,2,7),(6,3,9),(2,3,3)])
+def test_add(x,y,z):
     res = calculator.add(x,y)
-    assert res == x+y
+    assert res == z
 
-@pytest.mark.addsub
-def test_sub():
-    x = 10
-    y = 20
+@pytest.mark.parametrize("x,y,z",[(5,2,3),(6,3,3),(2,3,0)])
+def test_sub(x,y,z):
     res = calculator.sub(x, y)
-    assert res == x - y
+    assert res == z
 
-@pytest.mark.muldiv
-def test_mul():
-    x = 10
-    y = 20
+@pytest.mark.parametrize("x,y,z",[(5,2,10),(6,3,18),(2,3,4)])
+def test_mul(x,y,z):
     res = calculator.mul(x, y)
-    assert res == x * y
+    assert res == z
 
-@pytest.mark.muldiv
-def test_div():
-    x = 10
-    y = 20
+@pytest.mark.parametrize("x,y,z",[(6,2,3),(6,3,2),(10,5,1)])
+def test_div(x,y,z):
     res = calculator.div(x, y)
-    assert res == x / y
+    assert res == z
 
 
